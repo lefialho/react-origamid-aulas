52f22caa9088b8d70b92088ce7d972bb2ab78854
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2022-09-18 19:05:26 -0300

    Aula components checkbox e radio

---
+++ src/formularios/components/checkbox.js
@@ -0,0 +1,26 @@
+export function Checkbox({ options, value, setValue, ...props }) {
+  function handleChange({ target }) {
+    if (target.checked) {
+      setValue([...value, target.value]);
+    } else {
+      setValue(value.filter((itemValue) => itemValue !== target.value));
+    }
+  }
+
+  return (
+    <>
+      {options.map((option) => (
+        <label key={option}>
+          <input
+            type="checkbox"
+            value={option}
+            checked={value.includes(option)}
+            onChange={handleChange}
+            {...props}
+          />{' '}
+          {option}
+        </label>
+      ))}
+    </>
+  );
+}
--- src/formularios/components/index.js
+++ src/formularios/components/index.js
@@ -1,14 +1,45 @@
 import { useState } from 'react';
+import { Checkbox } from './checkbox';
 import { Input } from './input';
+import { Radio } from './radio';
 import { Select } from './select';
 
 export function FormComponents() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [select, setSelect] = useState('');
+  const [select, setSelect] = useState('Tablet');
+  const [sexo, setSexo] = useState('Masculino');
+  const [pagamento, setPagamento] = useState('Cartão');
+  const [interesse, setInteresse] = useState(['Tecnologia']);
+  const [termo, setTermo] = useState([]);
 
   return (
     <form action="">
+      <h2>Termos:</h2>
+      <Checkbox
+        options={['Li e aceito os termos']}
+        value={termo}
+        setValue={setTermo}
+      />
+      <h2>Sexo:</h2>
+      <Radio
+        options={['Masculino', 'Feminino']}
+        value={sexo}
+        setValue={setSexo}
+      />
+      <h2>Pagamento:</h2>
+      <Radio
+        options={['Cartão', 'Dinheiro', 'Pix']}
+        value={pagamento}
+        setValue={setPagamento}
+      />
+      <h2>Interesses</h2>
+      <Checkbox
+        options={['Saúde', 'Tecnologia', 'Culinária']}
+        value={interesse}
+        setValue={setInteresse}
+      />
+      <h2>Interesses</h2>
       <Select
         options={['Smartphone', 'Tablet', 'Notebook']}
         value={select}
@@ -30,7 +61,7 @@ export function FormComponents() {
         setValue={setEmail}
         required
       />
-      <button>Enviar</button>
+      <button disabled={termo.length === 0}>Enviar</button>
     </form>
   );
 }
